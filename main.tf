8ce0fcdc35f65136be48a662e5c4bdf4a5dfb1a7
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2025-12-11 13:37:23 -0800

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
